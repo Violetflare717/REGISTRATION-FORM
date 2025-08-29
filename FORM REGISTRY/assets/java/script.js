230b4015bea98e64528e7d97c4d224a6a78c9abb
Violetflare Mayfield <42115841+Violetflare717@users.noreply.github.com>
2025-08-29 10:42:12 +0800

    FORM REGISTRATION DONE ON AUGUST OF 15TH 2025 FRI

---
+++ FORM REGISTRY/assets/java/script.js
@@ -0,0 +1,4 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page refresh
+    alert("Registration is done!");
+});
