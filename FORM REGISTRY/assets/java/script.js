cb3edcc7b094ee5a924caeffdde7c21060af1f3f
Violetflare Mayfield <42115841+Violetflare717@users.noreply.github.com>
2025-09-05 10:42:32 +0800

    Delete FORM REGISTRY directory
    I MADE THE MISTAKE OF PLACING THE ENTIRE FOLDER INSTEAD OF JUST THE INDEX AND ASSETS AND STUFF I AM SO COOKED WITH THIS ONE FR

--- FORM REGISTRY/assets/java/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent page refresh
-    alert("Registration is done!");
-});
